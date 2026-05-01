6b4787c04b59db1b34d5830d480cf288a05501b6
Harsha M S <harsha.ms@citrix.com>
2026-05-01 14:25:18 +0530

    Fix the menu icon in mobile

--- src/config.ts
+++ src/config.ts
@@ -24,12 +24,12 @@ export type SheetKey =
 
 export const SHEET_URLS: Record<SheetKey, string> = {
   siteContent:  'https://docs.google.com/spreadsheets/d/1RvpGb7NdC4vrZW0RyEru3yP9JeH310TmW6VTkLMV8YE/edit',
-  banners:      'https://docs.google.com/spreadsheets/d/13TPRmOTUcKyJQkJvpErLhpnv65mDfkYA97LwXxX-IvQ/edit?gid=0#gid=0',
-  team:         'https://docs.google.com/spreadsheets/d/1o7ap8DtkO5SDTEzBp_7hzcW2k1zobTdYWls5coWBeuA/edit?gid=0#gid=0',
-  temples:      'https://docs.google.com/spreadsheets/d/1L7XH5_A-4hKAaKIjeFTV9-a99_n5Cl1tvIaenK_yE7g/edit?gid=0#gid=0',
-  hostels:      'https://docs.google.com/spreadsheets/d/1Q6iB7_nhaFGbvzIT7N9bhXEC5oM6_djzugwVOuuhV6I/edit?gid=0#gid=0',
-  achievements: 'https://docs.google.com/spreadsheets/d/1JkqgQN79pS7oCIcnLMKBzYhQg-oTrWmMm0_bprOxB-U/edit?gid=0#gid=0',
-  schemes:      'https://docs.google.com/spreadsheets/d/1wkglcllORYWFAM9-C3EigJ03YhiEk61ImUCYFZMLf2Q/edit?gid=0#gid=0',
+  banners:      'https://docs.google.com/spreadsheets/d/13TPRmOTUcKyJQkJvpErLhpnv65mDfkYA97LwXxX-IvQ/edit',
+  team:         'https://docs.google.com/spreadsheets/d/1o7ap8DtkO5SDTEzBp_7hzcW2k1zobTdYWls5coWBeuA/edit',
+  temples:      'https://docs.google.com/spreadsheets/d/1L7XH5_A-4hKAaKIjeFTV9-a99_n5Cl1tvIaenK_yE7g/edit',
+  hostels:      'https://docs.google.com/spreadsheets/d/1Q6iB7_nhaFGbvzIT7N9bhXEC5oM6_djzugwVOuuhV6I/edit',
+  achievements: 'https://docs.google.com/spreadsheets/d/1JkqgQN79pS7oCIcnLMKBzYhQg-oTrWmMm0_bprOxB-U/edit',
+  schemes:      'https://docs.google.com/spreadsheets/d/1wkglcllORYWFAM9-C3EigJ03YhiEk61ImUCYFZMLf2Q/edit',
   roadmap:      'https://docs.google.com/spreadsheets/d/1Q9yPiH2tTktC8swQve_bPSebu5XDwirTpVI5fgejaV0/edit',
 };
 
